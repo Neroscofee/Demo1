f8a97f830608f090d5026bd37ce23dac042452f9
shifeilong <shifeilong@fang.com>
2017-08-29 14:22:52 +0800

    添加项目文件。

---
+++ Demo1/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo1
+{
+    class Person
+    {
+        public int Value;
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Demo1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Demo1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Person xm, xn;
+            xm = new Person();
+            xm.Value = 6;
+            xn = xm;
+            Console.WriteLine(xn.Value);
+            xn.Value = 9;
+            Console.WriteLine(xm.Value);
+
+            Console.WriteLine(xm.Add(4, 7));
+            Console.ReadKey();
+        }
+    }
+}
