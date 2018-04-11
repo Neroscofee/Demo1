3cdd97d0c416531094b296180e6779fccc25a9f2
Neroscofee <Neroscofee@163.com>
2018-04-11 18:32:31 +0800

    1
    1

---
+++ Comm/Json.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comm
+{
+    public class Json
+    {
+        #region 任意类型转Json
+
+        public static string ListToJson<T>(IList<T> list,string jsonName)
+        {
+            StringBuilder Json = new StringBuilder();
+            if (string.IsNullOrEmpty(jsonName))
+            {
+                jsonName = list[0].GetType().Name;
+            }
+            Json.Append("{\""+jsonName+"\":[");
+            if (list.Count >0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    T obj = Activator.CreateInstance<T>();
+                    PropertyInfo[] pi = obj.GetType().GetProperties();
+                    Json.Append("{");
+                    for (int j = 0; j < pi.Length; j++)
+                    {
+                        Type type = pi[j].GetValue(list[i], null).GetType();
+                        Json.Append("\"" + pi[j].Name.ToString() + "\":" + StringFormat(pi[j].GetValue(list[i], null).ToString(), type));
+                        if (j < pi.Length-1)
+                        {
+                            Json.Append(",");
+                        }
+                    }
+                    Json.Append("}");
+                    if (i < list.Count-1)
+                    {
+                        Json.Append(",");
+                    }
+                }
+            }
+            Json.Append("]}");
+            return Json.ToString();
+
+        }
+
+
+
+
+
+        #endregion
+
+
+
+
+        /// <summary>
+        /// 格式化字符型、日期型、布尔型
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string StringFormat(string str,Type type)
+        {
+            if (type != typeof(string) && string.IsNullOrEmpty(str) )
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(string))
+            {
+                str = FliterSpecilCharacter(str);
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + str.Split(' ')[0] + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+            return str;
+        }
+        /// <summary>
+        /// 过滤特殊字符
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private static string FliterSpecilCharacter(String s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s.ToCharArray()[i];
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\"");
+                        break;
+                    case '\\':
+                        sb.Append("\\\\");
+                        break;
+                    case '/':
+                        sb.Append("\\/");
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+                    default:
+                        sb.Append(c);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+
+
+
+
+
+    }
+}
