c22e61cbff89731fb30cdf1996b2493c7b556460
Neroscofee <Neroscofee@163.com>
2018-05-14 18:18:17 +0800

    utility
    utility

--- Comm/Json.cs
+++ Comm/Json.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -10,7 +11,13 @@ namespace Comm
     public class Json
     {
         #region 任意类型转Json
-
+        /// <summary>
+        /// List转json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="jsonName"></param>
+        /// <returns></returns>
         public static string ListToJson<T>(IList<T> list,string jsonName)
         {
             StringBuilder Json = new StringBuilder();
@@ -46,7 +53,72 @@ namespace Comm
             return Json.ToString();
 
         }
+        /// <summary>
+        /// List转Json
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static string ListToJson<T>(IList<T> list)
+        {
+            object obj = list[0];
+            return ListToJson<T>(list, obj.GetType().Name);
+        }
+        /// <summary>
+        /// Datatable转Json
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string DataTableToJson(DataTable dt)
+        {
+            StringBuilder jsonString = new StringBuilder();
+            jsonString.Append("[");
+            DataRowCollection drc = dt.Rows;
+            for (int i = 0; i < drc.Count; i++)
+            {
+                jsonString.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string strKey = dt.Columns[j].ColumnName;
+                    string strValue = drc[i][j].ToString();
+                    Type type = dt.Columns[j].DataType;
+                    jsonString.Append("\"" + strKey + "\":");
+                    strValue = StringFormat(strValue, type);
+                    if (j<dt.Columns.Count-1)
+                    {
+                        jsonString.Append(strValue + ",");
+                    }
+                    else
+                    {
+                        jsonString.Append(strValue);
+                    }
+                }
 
+                jsonString.Append("},");
+
+            }
+            if (jsonString.ToString().Contains(","))
+            {
+                jsonString.Remove(jsonString.Length - 1, 1);
+            }
+            jsonString.Append("]");
+            return jsonString.ToString();
+        }
+        /// <summary>
+        /// DataSet转换为Json
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <returns></returns>
+        public static string DataSetToJson(DataSet dataSet)
+        {
+            string jsonString = "{";
+            foreach (DataTable table in dataSet.Tables)
+            {
+                jsonString += "\"" + table.TableName + "\":" + DataTableToJson(table) + ",";
+            }
+            jsonString = jsonString.Trim(',');
+            return jsonString + "}";//111
+        }
 
 
 
